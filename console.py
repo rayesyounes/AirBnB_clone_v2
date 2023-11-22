75549b79c8fbef65a08bdb4a4f7a3c9effa19516
hepona <hepona10@gmail.com>
2023-11-22 21:01:21 +0100

    test task2 double quote

--- console.py
+++ console.py
@@ -205,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
                 pass
 
         new_instance = HBNBCommand.classes[class_name](**dict_params)
-        if(new_instance.save()):
+        if new_instance.save():
             print(new_instance.id)
 
     def help_create(self):
@@ -294,7 +294,8 @@ class HBNBCommand(cmd.Cmd):
         else:
             for k, v in storage.all().items():
                 print_list.append(str(v))
-        print(print_list)
+        for obj in print_list:
+            print(obj)
 
     def help_all(self):
         """Help information for the all command"""
