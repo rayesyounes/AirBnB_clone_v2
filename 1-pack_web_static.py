80add60974670719a5ae200bcc0722073d0a1bc1
rayesyounes <rayesyounes88@gmail.com>
2023-12-07 22:16:36 +0100

    checker 3 fix ?

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,10 +4,14 @@
 from fabric.api import task, local
 from datetime import datetime
 
+
 @task
 def do_pack():
     """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
     """
     formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = f"versions/web_static_{formatted_dt}.tgz"
     print(f"Packing web_static to versions/web_static_{formatted_dt}.tgz")
-    local(f"mkdir -p versions && tar -cvzf versions/web_static_{formatted_dt}.tgz web_static")
+    local(f"{mkdir}&& tar -cvzf {path} web_static")
