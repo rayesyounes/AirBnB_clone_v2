746d462efe15a69def3e97ce0f46590d47120449
rayesyounes <rayesyounes88@gmail.com>
2023-12-07 22:49:46 +0100

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,14 +5,15 @@ from fabric.api import task, local, env, put, run
 from datetime import datetime
 import os
 
-env.hosts = ['18.207.1.87', '52.206.189.175']
+env.hosts = ['100.25.29.81', '100.25.204.214']
+
 
 @task
 def do_pack():
     """ method doc
         sudo fab -f 1-pack_web_static.py do_pack
     """
-    formatted_dt = f"$(date +%Y%m%d%H%M%S)"
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
     mkdir = "mkdir -p versions"
     path = f"versions/web_static_{formatted_dt}.tgz"
     print(f"Packing web_static to versions/web_static_{formatted_dt}.tgz")
@@ -22,15 +23,18 @@ def do_pack():
 @task
 def do_deploy(archive_path):
     """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
     """
-    if os.path.exists(archive_path):
+    try:
+        if os.path.exists(archive_path) is False:
+            return False
         fn_with_ext = os.path.basename(archive_path)
         fn_no_ext, ext = os.path.splitext(fn_with_ext)
         dpath = "/data/web_static/releases/"
-        private_key = env.key_filename
-        username = env.user
         put(archive_path, "/tmp/")
+        run(f"rm -rf {dpath}{fn_no_ext}/")
         run(f"mkdir -p {dpath}{fn_no_ext}/")
         run(f"tar -xzf /tmp/{fn_with_ext} -C {dpath}{fn_no_ext}/")
         run(f"rm /tmp/{fn_with_ext}")
@@ -40,5 +44,5 @@ def do_deploy(archive_path):
         run(f"ln -s {dpath}{fn_no_ext}/ /data/web_static/current")
         print("New version deployed!")
         return True
-    else:
+    except Exception:
         return False
